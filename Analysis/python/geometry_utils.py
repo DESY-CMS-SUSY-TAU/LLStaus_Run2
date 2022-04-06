f9d6a0e90a6e696111737fa359c10c8391b8e9f4
SohamBhattacharya <soham.elessar@gmail.com>
2022-04-06 16:21:55 +0200

    Added vertex transformation code.

--- Analysis/python/geometry_utils.py
+++ Analysis/python/geometry_utils.py
@@ -1,32 +1,64 @@
+import awkward
+import coffea
+#import coffea.nanoevents
+#import coffea.nanoevents.methods
+import numba
 import numpy
 
+from coffea.nanoevents.methods import candidate
+awkward.behavior.update(candidate.behavior)
 
 """
-CaloPoint implementation ported from:
+CaloPoint and CaloPoint3D implementation ported from:
 https://github.com/cms-sw/cmssw/blob/master/DataFormats/JetReco/src/Jet.cc
 """
 
-class CaloPoint :
+#@numba.jitclass
+#class CaloPoint :
+#    
+#    depth = 0.1
+#    R_BARREL = (1. - depth) * 143. + depth * 407.
+#    R_BARREL2 = R_BARREL * R_BARREL
+#    Z_ENDCAP = (1. - depth) * 320. + depth * 568.                         # 1/2(EEz+HEz)
+#    R_FORWARD = Z_ENDCAP / numpy.sqrt(numpy.cosh(3.) * numpy.cosh(3.) - 1.)  # eta=3
+#    R_FORWARD2 = R_FORWARD * R_FORWARD
+#    Z_FORWARD = 1100. + depth * 165.
+#    Z_BIG = 1.e5
+
+
+spec_CaloPoint3D = [
+    ("vertex", numba.float64[:]),
+    ("direction", numba.float64[:]),
+    ("point", numba.float64[:]),
     
-    depth = 0.1
-    R_BARREL = (1. - depth) * 143. + depth * 407.
-    R_BARREL2 = R_BARREL * R_BARREL
-    Z_ENDCAP = (1. - depth) * 320. + depth * 568.                         # 1/2(EEz+HEz)
-    R_FORWARD = Z_ENDCAP / numpy.sqrt(numpy.cosh(3.) * numpy.cosh(3.) - 1.)  # eta=3
-    R_FORWARD2 = R_FORWARD * R_FORWARD
-    Z_FORWARD = 1100. + depth * 165.
-    Z_BIG = 1.e5
+    ("depth", numba.float64),
+    ("R_BARREL", numba.float64),
+    ("R_BARREL2", numba.float64),
+    ("Z_ENDCAP", numba.float64),
+    ("R_FORWARD", numba.float64),
+    ("R_FORWARD2", numba.float64),
+    ("Z_FORWARD", numba.float64),
+    ("Z_BIG", numba.float64),
+]
 
-
-#new implementation to derive CaloPoint for free 3d vertex.
-#code provided thanks to Christophe Saout
-class CaloPoint3D(CaloPoint) :
+@numba.experimental.jitclass(spec_CaloPoint3D)
+class CaloPoint3D : #(CaloPoint) :
     
     def __init__(
         self,
         vertex,
         direction,
     ) :
+        
+        self.depth = 0.1
+        self.R_BARREL = (1. - self.depth) * 143. + self.depth * 407.
+        self.R_BARREL2 = self.R_BARREL * self.R_BARREL
+        self.Z_ENDCAP = (1. - self.depth) * 320. + self.depth * 568.                         # 1/2(EEz+HEz)
+        self.R_FORWARD = self.Z_ENDCAP / numpy.sqrt(numpy.cosh(3.) * numpy.cosh(3.) - 1.)  # eta=3
+        self.R_FORWARD2 = self.R_FORWARD * self.R_FORWARD
+        self.Z_FORWARD = 1100. + self.depth * 165.
+        self.Z_BIG = 1.e5
+        
         # note: no sanity checks here, make sure vertex is inside the detector!
     
         # check if positive or negative (or none) endcap should be tested
@@ -83,13 +115,19 @@ class CaloPoint3D(CaloPoint) :
         
     
         # rotate (r, tIP, z) back into original x-y coordinate system
-        self.point = numpy.array([
-            dirUnit[0] * r - dirUnit[1] * tIP,
-            dirUnit[1] * r + dirUnit[0] * tIP,
-            z
-        ])
-    
+        #self.point = numpy.array([
+        #    dirUnit[0] * r - dirUnit[1] * tIP,
+        #    dirUnit[1] * r + dirUnit[0] * tIP,
+        #    z,
+        #], dtype = numpy.float)
+        
+        self.point = numpy.zeros(3)
+        self.point[0] = dirUnit[0] * r - dirUnit[1] * tIP
+        self.point[1] = dirUnit[1] * r + dirUnit[0] * tIP
+        self.point[2] = z
 
+
+@numba.njit
 def getMagP3(v) :
     
     mag = numpy.sqrt(numpy.sum(v**2))
@@ -97,10 +135,11 @@ def getMagP3(v) :
     return mag
 
 
+@numba.njit
 def physicsP4(
-    newVertex, # (x, y, z)
     inParticle, # (px, py, pz, E)
-    oldVertex = (0.0, 0.0, 0.0), # (x, y, z)
+    oldVertex, # (x, y, z)
+    newVertex, # (x, y, z)
 ) :
     """
     Argument format:
@@ -111,16 +150,29 @@ def physicsP4(
         Will change the vertex of "inParticle" from "oldVertex" to "newVertex" and return the new p4(px, py, pz, E).
     """
     
-    newVertex = numpy.array(newVertex, dtype = float)
-    inParticle = numpy.array(inParticle, dtype = float)
-    oldVertex = numpy.array(oldVertex, dtype = float)
+    #arr = numpy.array(numba.typed.List([0.8, 0.6, -1.7]))
     
-    inParticle_p = numpy.array(inParticle[0: 3])
+    #print("inParticle:", inParticle)
+    #print("oldVertex:", oldVertex)
+    #print("newVertex:", newVertex)
+    
+    #inParticle = numpy.array(inParticle, dtype = float)
+    #oldVertex = numpy.array(oldVertex, dtype = float)
+    #newVertex = numpy.array(newVertex, dtype = float)
+    
+    inParticle = numpy.array([_val for _val in inParticle])#, dtype = float)
+    oldVertex = numpy.array([_val for _val in oldVertex])#, dtype = float)
+    newVertex = numpy.array([_val for _val in newVertex])#, dtype = float)
+    
+    ##inParticle_p = numpy.array(inParticle[0: 3])
+    inParticle_p = inParticle[0: 3].copy()
     
     # Jet position in Calo
     caloPoint = CaloPoint3D(
-        vertex = oldVertex,
-        direction = inParticle_p,
+        #vertex = oldVertex,
+        #direction = inParticle_p,
+        oldVertex,
+        inParticle_p,
     )
     
     physicsDir = caloPoint.point - newVertex
@@ -128,12 +180,232 @@ def physicsP4(
     physicsDir_unit = physicsDir / getMagP3(physicsDir)
     p3 = p * physicsDir_unit
     
-    physicsP4 = numpy.array([
+    resultP4 = numpy.array([
         p3[0],
         p3[1],
         p3[2],
         inParticle[3],
     ])
     
-    return physicsP4
+    return resultP4
 
+
+vphysicsP4 = numpy.vectorize(
+    pyfunc = physicsP4,
+    signature = "(4),(3),(3)->(4)",
+    #signature = "(4),(3),(nvtx,3)->(nvtx,4)",
+    #excluded = ["newVertex", "oldVertex"],
+)
+
+
+def numpy_to_coffeaLorentzVector(x, y, z, t) :
+    
+    lv = awkward.zip(
+        {
+            "x": x,
+            "y": y,
+            "z": z,
+            "t": t,
+        },
+        with_name = "LorentzVector"
+        #parameters = {"__record__": "LorentzVector"}
+    )
+    
+    return lv
+
+
+def get_p4_shiftVertex(
+    v1s,
+    v2s,
+    oldVertex = None,
+    newVertex = None,
+) :
+    """
+    Change the vertex of v2s from "oldVertex" to "newVertex" and return the deltaR w.r.t. v1s.
+    Will attempt to use (vertexX, vertexY, vertexZ) from v2s if oldVertex is not provided.
+    Will attempt to use (vertexX, vertexY, vertexZ) from v1s if newVertex is not provided.
+    Designed to work with the coffea "nearest" metric which broadcasts v1 and v2 such that,
+    v1s = [
+        [
+            [v1_0, v1_0, v1_0],
+            [v1_1, v1_1, v1_1]
+        ], # Event 0
+        ...
+    ]
+    
+    v2s = [
+        [
+            [v2_0, v2_1, v2_2],
+            [v2_0, v2_1, v2_2],
+        ], # Event 0
+        ...
+    ]
+    
+    when Event 0 has 2 entries for v1 and 3 entries for v2.
+    """
+    
+    builder = awkward.ArrayBuilder()
+    
+    assert(len(v1s) == len(v2s))
+    
+    for iEvt in range(len(v1s)) :
+        
+        with builder.list() :
+            
+            vv1s = v1s[iEvt]
+            vv2s = v2s[iEvt]
+            
+            assert(len(vv1s) == len(vv2s))
+            
+            for iv in range(len(vv1s)) :
+                
+                vvv1s = vv1s[iv]
+                vvv2s = vv2s[iv]
+                
+                assert(len(vvv1s) == len(vvv2s))
+                
+                v2_in = awkward.to_list(awkward.zip([vvv2s.x, vvv2s.y, vvv2s.z, vvv2s.t]))
+                oldVtxs = oldVertex
+                newVtxs = newVertex
+                
+                if (oldVtxs is None) :
+                    
+                    oldVtxs = awkward.to_list(awkward.zip([vvv2s.vertexX, vvv2s.vertexY, vvv2s.vertexZ]))
+                
+                if (newVtxs is None) :
+                    
+                    newVtxs = awkward.to_list(awkward.zip([vvv1s.vertexX, vvv1s.vertexY, vvv1s.vertexZ]))
+                
+                v2_shifted = vphysicsP4(
+                    inParticle = v2_in,
+                    oldVertex = oldVertex,
+                    newVertex = newVtxs,
+                )
+                
+                with builder.list() :
+                    
+                    for v2_tmp in v2_shifted :
+                        
+                        builder.begin_record("LorentzVector")
+                        builder.field("x").real(v2_tmp[0])
+                        builder.field("y").real(v2_tmp[1])
+                        builder.field("z").real(v2_tmp[2])
+                        builder.field("t").real(v2_tmp[3])
+                        builder.end_record()
+    
+    result = builder.snapshot()
+    
+    return result
+
+
+def coffea_nearest_metric_deltaR_shiftVertex(
+    v1s,
+    v2s,
+    oldVertex = None,
+    newVertex = None,
+) :
+    """
+    Designed to work as a 
+    """
+    
+    v2s_shifted = get_p4_shiftVertex(
+        v1s,
+        v2s,
+        oldVertex,
+        newVertex,
+    )
+    
+    result = v1s.delta_r(v2s_shifted)
+    #print("delta_r:", result)
+    
+    return result
+
+
+#def get_deltaR_shiftVertex(
+#    v1s,
+#    v2s,
+#    oldVertex = None,
+#    newVertex = None,
+#) :
+#    """
+#    Change the vertex of v2s from "oldVertex" to "newVertex" and return the deltaR w.r.t. v1s.
+#    Will attempt to use (vertexX, vertexY, vertexZ) from v2s if oldVertex is not provided.
+#    Will attempt to use (vertexX, vertexY, vertexZ) from v1s if newVertex is not provided.
+#    """
+#    
+#    builder = awkward.ArrayBuilder()
+#    
+#    assert(len(v1s) == len(v2s))
+#    
+#    for iEvt in range(len(v1s)) :
+#        
+#        builder.begin_list()
+#        
+#        vv1s = v1s[iEvt]
+#        vv2s = v2s[iEvt]
+#        
+#        assert(len(vv1s) == len(vv2s))
+#        
+#        for iv in range(len(vv1s)) :
+#            
+#            vvv1s = vv1s[iv]
+#            vvv2s = vv2s[iv]
+#            
+#            assert(len(vvv1s) == len(vvv2s))
+#            
+#            #print("vvv1s:", vvv1s)
+#            #print("vvv2s:", vvv2s)
+#            #print(len(vvv1s), len(vvv2s))
+#            
+#            v2_in = awkward.to_list(awkward.zip([vvv2s.x, vvv2s.y, vvv2s.z, vvv2s.t]))
+#            oldVtxs = oldVertex
+#            newVtxs = newVertex
+#            
+#            if (oldVtxs is None) :
+#                
+#                oldVtxs = awkward.to_list(awkward.zip([vvv2s.vertexX, vvv2s.vertexY, vvv2s.vertexZ]))
+#            
+#            if (newVtxs is None) :
+#                
+#                newVtxs = awkward.to_list(awkward.zip([vvv1s.vertexX, vvv1s.vertexY, vvv1s.vertexZ]))
+#            
+#            print("v2_in:", v2_in)
+#            #print("newVtxs:", newVtxs)
+#            
+#            v2_shifted = vphysicsP4(
+#                inParticle = v2_in,
+#                oldVertex = oldVertex,
+#                newVertex = newVtxs,
+#            )
+#            
+#            print("v2_shifted:", v2_shifted)
+#            
+#            v2_shiftedP4s = awkward.zip(
+#                {
+#                    "x": v2_shifted[:, 0],
+#                    "y": v2_shifted[:, 1],
+#                    "z": v2_shifted[:, 2],
+#                    "t": v2_shifted[:, 3],
+#                },
+#                with_name = "LorentzVector"
+#            )
+#            
+#            #print("v2_shiftedP4s:", v2_shiftedP4s)
+#            
+#            arr_dR = vvv1s.delta_r(v2_shiftedP4s)
+#            #print("arr_dR:", arr_dR)
+#            
+#            builder.begin_list()
+#            
+#            for dR in arr_dR :
+#                
+#                builder.real(dR)
+#            
+#            builder.end_list()
+#        
+#        builder.end_list()
+#    
+#    result = builder.snapshot()
+#    print("result:", result)
+#    
+#    return result
--- Analysis/python/test_geomutils.py
+++ Analysis/python/test_geomutils.py
@@ -1,23 +1,226 @@
-import geometry_utils
+import aghast
+import awkward
+import boost_histogram
+import coffea
+import coffea.hist
+import coffea.processor
+import dataclasses
+import logging
+import numba
+import numpy
+import os
+import sortedcontainers
+import uproot3
+
 import ROOT
+ROOT.gROOT.SetBatch(True)
+
+#import CMS_lumi, tdrstyle
+import geometry_utils
+import utils
+
+from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
 
 
-newVertex = (20.0, 20.0, 0.0)
-detP4_PtEtaPhiM = (50.0, 0.7, 1.9, 10.4)
-oldVertex = (0.0, 0.0, 0.0)
+newVertex = [20.0, 20.0, 0.0]
+detP4_PtEtaPhiM = [50.0, 0.45, 1.9, 10.4]
+oldVertex = [0.0, 0.0, 0.0]
 
 rt_detP4 = ROOT.Math.PtEtaPhiMVector(*detP4_PtEtaPhiM)
 
-detP4 = (rt_detP4.Px(), rt_detP4.Py(), rt_detP4.Pz(), rt_detP4.E())
+detP4 = [rt_detP4.Px(), rt_detP4.Py(), rt_detP4.Pz(), rt_detP4.E()]
 
-physP4 = geometry_utils.physicsP4(
-    newVertex = newVertex,
-    inParticle = detP4,
-    oldVertex = oldVertex,
-)
+#l_physP4 = [geometry_utils.physicsP4(
+#    newVertex = newVertex,
+#    inParticle = detP4,
+#    oldVertex = oldVertex,
+#)]
 
-rt_phP4 = ROOT.Math.PxPyPzEVector(*physP4)
+#l_physP4 = [geometry_utils.physicsP4(
+#    newVertex = newVertex,
+#    inParticle = detP4,
+#    oldVertex = oldVertex,
+#)]
+
+#l_newVertex = [newVertex]
+#l_detP4 = [detP4]
+#l_oldVertex = [oldVertex]
+#
+#l_physP4 = geometry_utils.vphysicsP4( 
+#    newVertex = l_newVertex,
+#    inParticle = l_detP4,
+#    oldVertex = l_oldVertex,
+#)
+#
+##print(l_physP4)
+#
+#for iVec, physP4 in enumerate(l_physP4) :
+#    
+#    print(physP4)
+#    rt_phP4 = ROOT.Math.PxPyPzEVector(*physP4)
+#    
+#    print("[vec %d] Old: pt %0.4f, eta %+0.4f, phi %+0.4f, mass %0.4f, energy %0.4f" %(iVec, rt_detP4.Pt(), rt_detP4.Eta(), rt_detP4.Phi(), rt_detP4.M(), rt_detP4.E()))
+#    print("[vec %d] New: pt %0.4f, eta %+0.4f, phi %+0.4f, mass %0.4f, energy %0.4f" %(iVec, rt_phP4.Pt(), rt_phP4.Eta(), rt_phP4.Phi(), rt_phP4.M(), rt_phP4.E()))
+#    print()
 
 
-print("Old: pt %0.4f, eta %+0.4f, phi %+0.4f, mass %0.4f, energy %0.4f" %(rt_detP4.Pt(), rt_detP4.Eta(), rt_detP4.Phi(), rt_detP4.M(), rt_detP4.E()))
-print("New: pt %0.4f, eta %+0.4f, phi %+0.4f, mass %0.4f, energy %0.4f" %(rt_phP4.Pt(), rt_phP4.Eta(), rt_phP4.Phi(), rt_phP4.M(), rt_phP4.E()))
+@dataclasses.dataclass
+class MyProcessor(coffea.processor.ProcessorABC) :
+    
+    trigger_str         : str       = None
+    
+    def __post_init__(self) :
+        
+        self.dataset_axis = coffea.hist.Cat("dataset", "Dataset")
+        
+        self._accumulator = coffea.processor.dict_accumulator({
+            "MET_pt": coffea.hist.Hist(
+                "Counts",
+                self.dataset_axis,
+                coffea.hist.Bin("MET_pt", "MET_pt", 100, 0, 1000),
+            ),
+        })
+    
+    
+    @property
+    def accumulator(self) :
+        
+        return self._accumulator
+    
+    
+    # we will receive a NanoEvents instead of a coffea DataFrame
+    def process(self, events) :
+        
+        if (self.trigger_str is not None and len(self.trigger_str)) :
+            
+            #events = events[events.HLT.PFMETNoMu110_PFMHTNoMu110_IDTight | events.HLT.PFMETNoMu120_PFMHTNoMu120_IDTight | ]
+            events = events[eval(self.trigger_str)]
+        
+        events = events[0: 5]
+        
+        events["GenVisTau", "vertexX"] = events.GenPart.vertexX[events.GenVisTau.genPartIdxMother]
+        events["GenVisTau", "vertexY"] = events.GenPart.vertexY[events.GenVisTau.genPartIdxMother]
+        events["GenVisTau", "vertexZ"] = events.GenPart.vertexZ[events.GenVisTau.genPartIdxMother]
+        
+        output = self.accumulator.identity()
+        
+        #print(events.GenVisTau.x)
+        
+        z_caloJetP4 = awkward.zip([events.CaloJet.x, events.CaloJet.y, events.CaloJet.z, events.CaloJet.t])
+        l_caloJetP4 = awkward.to_list(z_caloJetP4)
+        
+        #print(l_caloJetP4)
+        print(l_caloJetP4[0])
+        
+        #l_physP4 = geometry_utils.vphysicsP4(
+        #    inParticle = l_caloJetP4[0],
+        #    oldVertex = (0.0, 0.0, 0.0),
+        #    newVertex = (10.0, -10.0, 0.0)#, (-20.0, 5.0, 5.0)],
+        #)
+        #
+        #print(l_physP4)
+        
+        print("*"*10, len(events))
+        print("$"*10, len(events.GenVisTau))
+        print("$"*10, len(events.CaloJet))
+        print("#"*10, len(events.GenVisTau[0]))
+        print("#"*10, len(events.CaloJet[0]))
+        print("%"*10, len(events.GenVisTau[1]))
+        print("%"*10, len(events.CaloJet[1]))
+        
+        nearest = events.GenVisTau.nearest(
+            events.CaloJet,
+            #axis=1,
+            #metric = lambda v1, v2: geometry_utils.get_deltaR_shiftVertex(v1s = v1, v2s = v2, oldVertex = (0.0, 0.0, 0.0), newVertex = (10.0, -10.0, 0.0)),
+            
+            #metric = lambda v1, v2: geometry_utils.get_deltaR_shiftVertex(v1s = v1, v2s = v2, oldVertex = (0.0, 0.0, 0.0), newVertex = (0.0, 0.0, 0.0)),
+            #metric = lambda v1, v2: geometry_utils.coffea_nearest_metric_deltaR_shiftVertex(v1s = v1, v2s = v2, oldVertex = (0.0, 0.0, 0.0), newVertex = (0.0, 0.0, 0.0)),
+            
+            #metric = lambda v1, v2: geometry_utils.get_deltaR_shiftVertex(v1s = v1, v2s = v2, oldVertex = (0.0, 0.0, 0.0)),
+            metric = lambda v1, v2: geometry_utils.coffea_nearest_metric_deltaR_shiftVertex(v1s = v1, v2s = v2, oldVertex = (0.0, 0.0, 0.0)),
+            
+            return_metric = True,
+        )
+        
+        print(nearest[0])
+        print(nearest[1])
+        
+        return output
+    
+    
+    def postprocess(self, accumulator):
+        
+        return accumulator
+
+
+def main() :
+    
+    datasets = sortedcontainers.SortedDict({
+        "test_sample": ["tmp/nanoaod.root"],
+    })
+    
+    #trigger_str = ""
+    
+    trigger_str = (
+        "HLT_PFMET120_PFMHT120_IDTight"
+        " | HLT_PFMET130_PFMHT130_IDTight "
+        " | HLT_PFMET140_PFMHT140_IDTight "
+        " | HLT_PFMETNoMu110_PFMHTNoMu110_IDTight "
+        " | HLT_PFMETNoMu120_PFMHTNoMu120_IDTight "
+        " | HLT_PFMETNoMu130_PFMHTNoMu130_IDTight "
+        " | HLT_PFMETNoMu140_PFMHTNoMu140_IDTight "
+        
+        " | HLT_IsoMu24 "
+        #" | HLT_TkMu100 "
+        
+        #" | HLT_IsoMu27_MET90 "
+        
+        #" | HLT_DoubleTightChargedIsoPFTau35_Trk1_TightID_eta2p1_Reg "
+        #" | HLT_DoubleTightChargedIsoPFTau40_Trk1_eta2p1_Reg "
+        #" | HLT_DoubleMediumChargedIsoPFTau40_Trk1_TightID_eta2p1_Reg "
+        #" | HLT_DoubleMediumChargedIsoPFTauHPS35_Trk1_eta2p1_Reg "
+        
+        " | HLT_MediumChargedIsoPFTau50_Trk30_eta2p1_1pr_MET100 "
+        " | HLT_MediumChargedIsoPFTau50_Trk30_eta2p1_1pr_MET110 "
+        " | HLT_MediumChargedIsoPFTau50_Trk30_eta2p1_1pr_MET120 "
+        " | HLT_MediumChargedIsoPFTau50_Trk30_eta2p1_1pr_MET130 "
+        " | HLT_MediumChargedIsoPFTau50_Trk30_eta2p1_1pr_MET140 "
+        
+        " | HLT_MET105_IsoTrk50 "
+        " | HLT_MET120_IsoTrk50 "
+        
+        #" | HLT_IsoMu20_eta2p1_LooseChargedIsoPFTauHPS27_eta2p1_CrossL1 "
+        #" | HLT_IsoMu24_eta2p1_MediumChargedIsoPFTauHPS35_Trk1_eta2p1_Reg_CrossL1 "
+        
+        #" | HLT_Mu43NoFiltersNoVtx_Photon43_CaloIdL"
+        
+        #" | HLT_IsoTrackHB "
+    )
+    
+    trigger_str = trigger_str.replace("HLT_", "events.HLT.")
+    
+    mySchema = NanoAODSchema
+    
+    mySchema.mixins.update({
+        "CaloJet": "PtEtaPhiMCollection",
+    })
+    
+    output_num = coffea.processor.run_uproot_job(
+        datasets,
+        "Events",
+        MyProcessor(
+            #trigger_str = trigger_str,
+        ),
+        coffea.processor.iterative_executor,
+        #{"schema": NanoAODSchema},
+        {"schema": mySchema},
+    )
+    
+    
+    return 0
+
+
+if __name__ == "__main__" :
+    
+    main()
+
