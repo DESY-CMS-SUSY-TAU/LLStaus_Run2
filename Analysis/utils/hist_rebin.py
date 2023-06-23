88195f2848026a3b08925505baaca667e92edc9c
Mykyta Shchedrolosiev <mykytaua@naf-cms24.desy.de>
2023-06-23 19:37:53 +0200

    Small update to fake rate and hist_rebin

--- Analysis/utils/hist_rebin.py
+++ Analysis/utils/hist_rebin.py
@@ -1,116 +1,119 @@
 import numpy as np
 import ROOT
+import unittest
 
 class TH3Histogram:
-    def __init__(self, existing_hist, new_bin_edges_x, new_bin_edges_y, new_bin_edges_z):
-        self.bin_edges_x = np.array(new_bin_edges_x, dtype=np.float32)
-        self.bin_edges_y = np.array(new_bin_edges_y, dtype=np.float32)
-        self.bin_edges_z = np.array(new_bin_edges_z, dtype=np.float32)
-        self.bins = self._rebin(existing_hist, self.bin_edges_x, self.bin_edges_y, self.bin_edges_z)
-    
-    def _rebin(self, existing_hist, new_bin_edges_x, new_bin_edges_y, new_bin_edges_z):
-        existing_bin_edges_x = np.array([existing_hist.GetXaxis().GetBinLowEdge(bin) for bin in range(1, existing_hist.GetNbinsX() + 2)], dtype=np.float32)
-        existing_bin_edges_y = np.array([existing_hist.GetYaxis().GetBinLowEdge(bin) for bin in range(1, existing_hist.GetNbinsY() + 2)], dtype=np.float32)
-        existing_bin_edges_z = np.array([existing_hist.GetZaxis().GetBinLowEdge(bin) for bin in range(1, existing_hist.GetNbinsZ() + 2)], dtype=np.float32)
+    def __init__(self, hist, new_bin_edges_x, new_bin_edges_y, new_bin_edges_z):
+        self._hist = hist
+        self._new_bin_edges_x = np.array(new_bin_edges_x, dtype=np.float32)
+        self._new_bin_edges_y = np.array(new_bin_edges_y, dtype=np.float32)
+        self._new_bin_edges_z = np.array(new_bin_edges_z, dtype=np.float32)
+        self._rebin()
+
+    def _rebin(self):
+
+        old_bin_edges_x = np.array([self._hist.GetXaxis().GetBinLowEdge(bin) for bin in range(1, self._hist.GetNbinsX() + 2)], dtype=np.float32)
+        old_bin_edges_y = np.array([self._hist.GetYaxis().GetBinLowEdge(bin) for bin in range(1, self._hist.GetNbinsY() + 2)], dtype=np.float32)
+        old_bin_edges_z = np.array([self._hist.GetZaxis().GetBinLowEdge(bin) for bin in range(1, self._hist.GetNbinsZ() + 2)], dtype=np.float32)
         
-        if not (np.isin(new_bin_edges_x, existing_bin_edges_x).all()):
-            print("OLD bins:", existing_bin_edges_x)
-            print("NEW bins:", new_bin_edges_x)
-            print(np.isin(new_bin_edges_x, existing_bin_edges_x))
+        if not (np.isin(self._new_bin_edges_x, old_bin_edges_x).all()):
+            print("OLD bins:", old_bin_edges_x)
+            print("NEW bins:", self._new_bin_edges_x)
+            print(np.isin(self._new_bin_edges_x, old_bin_edges_x))
             raise ValueError("The new bin edges for X-axis are not equal to one of the existing histogram's bin edges.")
         
-        if not (np.isin(new_bin_edges_y, existing_bin_edges_y).all()):
-            print("OLD bins:", existing_bin_edges_y)
-            print("NEW bins:", new_bin_edges_y)
-            print(np.isin(new_bin_edges_y, existing_bin_edges_y))
+        if not (np.isin(self._new_bin_edges_y, old_bin_edges_y).all()):
+            print("OLD bins:", old_bin_edges_y)
+            print("NEW bins:", self._new_bin_edges_y)
+            print(np.isin(self._new_bin_edges_y, old_bin_edges_y))
             raise ValueError("The new bin edges for Y-axis are not equal to one of the existing histogram's bin edges.")
         
-        if not (np.isin(new_bin_edges_z, existing_bin_edges_z).all()):
-            print("OLD bins:", existing_bin_edges_z)
-            print("NEW bins:", new_bin_edges_z)
-            print(np.isin(new_bin_edges_z, existing_bin_edges_z))
+        if not (np.isin(self._new_bin_edges_z, old_bin_edges_z).all()):
+            print("OLD bins:", old_bin_edges_z)
+            print("NEW bins:", self._new_bin_edges_z)
+            print(np.isin(self._new_bin_edges_z, old_bin_edges_z))
             raise ValueError("The new bin edges for Z-axis are not equal to one of the existing histogram's bin edges.")
         
-        
-        n_bins_x = len(new_bin_edges_x) - 1
-        n_bins_y = len(new_bin_edges_y) - 1
-        n_bins_z = len(new_bin_edges_z) - 1
-        
-        new_bins = np.zeros((n_bins_x, n_bins_y, n_bins_z))
-        
-        for i in range(n_bins_x):
-            for j in range(n_bins_y):
-                for k in range(n_bins_z):
-                    x_indices = range(existing_hist.GetXaxis().FindBin(new_bin_edges_x[i]), existing_hist.GetXaxis().FindBin(new_bin_edges_x[i + 1]))
-                    y_indices = range(existing_hist.GetYaxis().FindBin(new_bin_edges_y[j]), existing_hist.GetYaxis().FindBin(new_bin_edges_y[j + 1]))
-                    z_indices = range(existing_hist.GetZaxis().FindBin(new_bin_edges_z[k]), existing_hist.GetZaxis().FindBin(new_bin_edges_z[k + 1]))
-                    
-                    # Sum up the bin contents from the existing bins
-                    new_bins[i, j, k] = np.sum(existing_hist.GetBinContent(x, y, z) for x in x_indices for y in y_indices for z in z_indices)
-        
-        return new_bins
-    
-    def get_th3_histogram(self):
-        n_bins_x = len(self.bin_edges_x) - 1
-        n_bins_y = len(self.bin_edges_y) - 1
-        n_bins_z = len(self.bin_edges_z) - 1
-        
-        th3_hist = ROOT.TH3D("hist", "Rebinned TH3 Histogram", n_bins_x, self.bin_edges_x, n_bins_y, self.bin_edges_y, n_bins_z, self.bin_edges_z)
-        
-        for i in range(n_bins_x):
-            for j in range(n_bins_y):
-                for k in range(n_bins_z):
-                    th3_hist.SetBinContent(i + 1, j + 1, k + 1, self.bins[i, j, k])
-        
-        return th3_hist
-    
-if __name__ == "__main__":
-    
-    # Define the existing TH3D histogram
-    existing_hist = ROOT.TH3D("existing_hist", "Existing TH3 Histogram", 10, 0, 10, 10, 0, 10, 10, 0, 10)
 
-    # Fill the existing histogram with some values
-    for i in range(1, existing_hist.GetNbinsX() + 1):
-        for j in range(1, existing_hist.GetNbinsY() + 1):
-            for k in range(1, existing_hist.GetNbinsZ() + 1):
-                existing_hist.SetBinContent(i, j, k, i + j + k)
-    
-    # Define the new bin edges for rebinning
-    new_bin_edges_x = [0, 2, 4, 6, 10]
-    new_bin_edges_y = [0, 3, 6, 9, 10]
-    new_bin_edges_z = [0, 2, 4, 6, 8, 10]
+        # Create the rebinned histogram
+        rebinned_hist = ROOT.TH3D(
+            f"{self._hist.GetName()}_rebinned",
+            f"Rebinned TH3D Histogram",
+            len(self._new_bin_edges_x) - 1,
+            self._new_bin_edges_x,
+            len(self._new_bin_edges_y) - 1,
+            self._new_bin_edges_y,
+            len(self._new_bin_edges_z) - 1,
+            self._new_bin_edges_z
+        )
 
-    # Create an instance of the TH3Histogram class
-    rebinned_hist = TH3Histogram(existing_hist, new_bin_edges_x, new_bin_edges_y, new_bin_edges_z)
+        # Fill the rebinned histogram
+        for i in range(0, self._hist.GetNbinsX() + 2):
+            for j in range(0, self._hist.GetNbinsY() + 2):
+                for k in range(0, self._hist.GetNbinsZ() + 2):
+                    content = self._hist.GetBinContent(i, j, k)
+                    bin_x = self._hist.GetXaxis().GetBinCenter(i)
+                    bin_y = self._hist.GetYaxis().GetBinCenter(j)
+                    bin_z = self._hist.GetZaxis().GetBinCenter(k)
+                    rebinned_hist.Fill(bin_x, bin_y, bin_z, content)
 
-    # Get the rebinned TH3 histogram
-    rebinned_th3_hist = rebinned_hist.get_th3_histogram()
+        self._rebinned_hist = rebinned_hist
 
-    # Verify the rebinned histogram properties
-    assert rebinned_th3_hist.GetNbinsX() == len(new_bin_edges_x) - 1
-    assert rebinned_th3_hist.GetNbinsY() == len(new_bin_edges_y) - 1
-    assert rebinned_th3_hist.GetNbinsZ() == len(new_bin_edges_z) - 1
+    def get_rebinned_histogram(self):
+        return self._rebinned_hist
+    
+class TH3HistogramTest(unittest.TestCase):
+    def test_rebinning(self):
+        # Create a TH3D histogram for testing
+        hist = ROOT.TH3D("hist", "Original TH3D Histogram", 10, 0, 10, 10, 0, 10, 10, 0, 10)
+        
+        # Fill the histogram with some data
+        for i in range(100):
+            x = ROOT.gRandom.Uniform(0, 10)
+            y = ROOT.gRandom.Uniform(0, 10)
+            z = ROOT.gRandom.Uniform(0, 10)
+            hist.Fill(x, y, z)
+        
+        # [print(hist.GetXaxis().GetBinLowEdge(bin)) for bin in range(1, hist.GetNbinsX() + 2)]
+        
+        # Fill the underflow bin
+        hist.Fill(-1, -1, -1)
+        
+        # Fill the overflow bin
+        hist.Fill(11, 11, 11)
+        
+        # Define the new bin edges for rebinning
+        new_bin_edges_x = [0, 2, 4, 6, 8, 9]
+        new_bin_edges_y = [0, 2, 4, 6, 8, 10]
+        new_bin_edges_z = [0, 2, 4, 6, 8, 10]
+        
+        # Rebin the histogram
+        rebinned_hist = TH3Histogram(hist, new_bin_edges_x, new_bin_edges_y, new_bin_edges_z)
+        
+        # Get the rebinned TH3D histogram
+        rebinned_th3_hist = rebinned_hist.get_rebinned_histogram()
+        
+        # Check the dimensions of the rebinned histogram
+        self.assertEqual(rebinned_th3_hist.GetNbinsX(), len(new_bin_edges_x) - 1)
+        self.assertEqual(rebinned_th3_hist.GetNbinsY(), len(new_bin_edges_y) - 1)
+        self.assertEqual(rebinned_th3_hist.GetNbinsZ(), len(new_bin_edges_z) - 1)
+        
 
-    # Verify the bin contents in the rebinned histogram
-    for i in range(1, rebinned_th3_hist.GetNbinsX() + 1):
-        for j in range(1, rebinned_th3_hist.GetNbinsY() + 1):
-            for k in range(1, rebinned_th3_hist.GetNbinsZ() + 1):
-                bin_content_rebinned = rebinned_th3_hist.GetBinContent(i, j, k)
-                
-                # Calculate the expected bin content based on the rebinning
-                expected_bin_content = np.sum(
-                    existing_hist.GetBinContent(x, y, z)
-                    for x in range(existing_hist.GetXaxis().FindBin(new_bin_edges_x[i - 1]), existing_hist.GetXaxis().FindBin(new_bin_edges_x[i]))
-                    for y in range(existing_hist.GetYaxis().FindBin(new_bin_edges_y[j - 1]), existing_hist.GetYaxis().FindBin(new_bin_edges_y[j]))
-                    for z in range(existing_hist.GetZaxis().FindBin(new_bin_edges_z[k - 1]), existing_hist.GetZaxis().FindBin(new_bin_edges_z[k]))
-                )
-                
-                assert bin_content_rebinned == expected_bin_content
+        projection_before = hist.Project3D("x")
+        projection_after = rebinned_th3_hist.Project3D("x")
+        
+        print("X projection of initial histogram:")
+        print(projection_before.Print('all'))
+
+        print("X projection of rebinned histogram:")
+        print(projection_after.Print('all'))
+        
+        self.assertAlmostEqual(projection_before.Integral() + projection_before.GetBinContent(0) +  projection_before.GetBinContent(projection_before.GetNbinsX()+1),
+                               projection_after.Integral() + projection_after.GetBinContent(0) +  projection_after.GetBinContent(projection_after.GetNbinsX()+1)
+                               )
+        
+        
+
+if __name__ == '__main__':
     
-    print("X projection of rebinned histogram:")
-    print(rebinned_th3_hist.Project3D("x").Print('all'))
-    
-    print("X projection of initial histogram:")
-    print(existing_hist.Project3D("x").Print('all'))
-    
-    print("Test passed successfully!")
+    unittest.main()
